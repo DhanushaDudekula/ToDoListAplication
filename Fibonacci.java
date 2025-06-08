ff70fb79c051dafd1bf390b5f579e71678a7d2d8
DhanushaDudekula <dhanusha5689@gmail.com>
2025-06-08 20:25:18 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = 0, b = 1, c, n;
+        int x = 0, y = 1, z, n;
 
         System.out.print("Enter N: ");
         n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            System.out.print(a + " ");
-            c = a + b;
-            a = b;
-            b = c;
+            System.out.print(x+ " ");
+            z = x + y;
+            x = y;
+            y = z;
         }
 
         sc.close();
